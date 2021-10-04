9ef6c2eb71cae6db5380c610edfd0f2d101cd376
mamewotoko <mamewotoko@users.noreply.github.com>
2021-10-04 12:56:19 +0000

    deploy: d1de123278f40a8d9514702e487da2cf24bf0a39

---
+++ main.ml
@@ -0,0 +1,75 @@
+open Chartist
+
+
+let () =
+  let _ : Chartist.Line.t =
+    Chartist.Line.make ".ct-chart"
+      (data_t
+         ~labels:[| "Monday"; "Tuesday"; "Wednesday"; "Thursday"; "Friday" |]
+         ~series:[|
+           [| 12.; 9.; 7.; 8.; 5. |]
+         |]
+         ())
+      () in
+  let _ : Chartist.Bar.t =
+    Chartist.Bar.make ".ct-bar-chart"
+      (data_t
+         ~labels:[| "Monday"; "Tuesday"; "Wednesday"; "Thursday"; "Friday" |]
+         ~series:[|
+           [| 12.; 9.; 7.; 8.; 5. |];
+           [| 2.; 29.; 4.; 8.4; 34. |]
+         |]
+         ())
+        () in
+  let _ : Chartist.Line.t =
+    Chartist.Line.make ".ct-area-chart"
+      (data_t
+         ~labels:[| "Monday"; "Tuesday"; "Wednesday"; "Thursday"; "Friday" |]
+         ~series:[|
+           [| 2.; 29.; 4.; 8.4; 34. |]
+         |]
+         ())
+      ~opt:(opt_t
+              ~low:0.0
+              ~showArea:true
+              ())
+        () in
+  let _ : Chartist.Line.t =
+    Chartist.Line.make ".ct-biarea-chart"
+      (data_t
+         ~labels:[| "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8" |]
+         ~series:[|
+             [| 1.; 2.; 3.; 1.; -2.; 0.; 1.; 0. |];
+             [| -2.; -1.; -2.; -1.; -2.5; -1.; -2.; -1. |];
+             [| 0.; 0.; 0.; 1.; 2.; 2.5; 2.; 1. |];
+             [| 2.5; 2.; 1.; 0.5; 1.; 0.5; -1.; -2.5 |]
+           |]
+           ())
+      ~opt:(opt_t
+              ~high: 3.0
+              ~low: ~-.3.0
+              ~showArea:true
+              ~showLine:false
+              ~showPoint:false
+              ~fullWidth:true
+              ~axisX:(axis_t
+                        ~showLabel:true
+                        ~showGrid:true
+                        ())
+              ())
+      () in
+  (* let _ : Chartist.Pie.t =
+   *   Chartist.Pie.make ".ct-pie-chart"
+   *     (data_t
+   *        ~labels: [| "Bananas"; "Apples"; "Grapes" |]
+   *        ~series:[|
+   *          [| 20.; 15.; 40. |]
+   *        |]
+   *        ())
+   *     ~opt:(opt_t
+   *             ~labelInterpolationFnc: (fun x -> x)
+   *             ())
+   *     ()
+   *   in *)
+    ()
+;;
